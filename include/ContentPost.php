a33edd7f1a62847c33f32817a5504b2ca4774d71
Emi <pieront.emilie@gmail.com>
2020-12-02 12:11:25 +0100

    modif signature centrée sur contentPost

--- include/ContentPost.php
+++ include/ContentPost.php
@@ -143,8 +143,8 @@
             <div class="content" style="position:relative;">
             <p class="col-12 m-0 divider p-2" <?= $postRow["postId"]; ?>><?= $postRow["postContent"]; ?></p>
         
-            <div class="d-flex align-items-center justify-center">
-                <p class="signature mt-3"><?= $author["userSignature"]; ?> </p>
+            <div class="text-center">
+                <p class="signature mt-4"><?= $author["userSignature"]; ?> </p>
             </div>
             
             </div> <!-- END OF CONTENT BOX-->
