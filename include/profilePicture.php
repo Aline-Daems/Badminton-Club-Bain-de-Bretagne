49811b17660653bca6483324f18cd36cd3d9aa81
Emi <pieront.emilie@gmail.com>
2020-12-10 19:07:09 +0100

    modif

--- include/profilePicture.php
+++ include/profilePicture.php
@@ -5,6 +5,11 @@ if (isset($_POST['upload'])) {
 
     $tmp_name= $_FILES['file']['tmp_name'];
 
+    $position= strpos($name, ".");
+
+    $fileextension= substr($name, $position + 1);
+
+    $fileextension= strtolower($fileextension);
 
     if (empty($name)){
         echo "Please choose a file";
